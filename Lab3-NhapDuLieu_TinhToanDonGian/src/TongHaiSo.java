a565b5053a745292e9459cb44c42cd0000e58f1b
NguyenGiaNhanGianTuyetTrinh-png <trinh.ngngt.cc25tth@ntu.edu.vn>
2026-08-03 00:00:10 +0700

    Hoàn thiện lab3- Nhập và tính toán

--- Lab3-NhapDuLieu_TinhToanDonGian/src/TongHaiSo.java
+++ Lab3-NhapDuLieu_TinhToanDonGian/src/TongHaiSo.java
@@ -13,7 +13,21 @@ public class TongHaiSo {
 			num1 = BanPhim.nextDouble();
 		System.out.print("Mời nhập số thứ hai:");
 			num2 = BanPhim.nextDouble();
-			
+		
+		// Tính toán
+		sum = num1+num2;
+		
+		// Xuất ra
+		//System.out.print("Tổng của " + num1 + "và" + num2 + "là" + sum);
+		//System.out.print("Tổng của ");
+		//System.out.print(num1);
+		//System.out.print(" + ");
+		//System.out.print(num2);
+		//System.out.print(" = ");
+		//System.out.print(sum);
+		
+		System.out.print("Tổng của %.2f và %.2f là %.2f", num1, num2, sum);
 	}
+	
 
 }
