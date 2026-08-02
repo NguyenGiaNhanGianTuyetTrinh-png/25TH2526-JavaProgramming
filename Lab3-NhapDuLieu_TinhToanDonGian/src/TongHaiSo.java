89b65585dc2bfc1e5819ed5b8bc2f02da0b778a9
NguyenGiaNhanGianTuyetTrinh-png <trinh.ngngt.cc25tth@ntu.edu.vn>
2026-08-02 23:41:01 +0700

    Thêm mã nhập, khai báo biến

--- Lab3-NhapDuLieu_TinhToanDonGian/src/TongHaiSo.java
+++ Lab3-NhapDuLieu_TinhToanDonGian/src/TongHaiSo.java
@@ -1,9 +1,19 @@
-
+import java.util.Scanner;
 public class TongHaiSo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Khai báo
+		double num1, num2, sum;
+		
+		// Khai báo đối tượng Sanner
+		Scanner BanPhim = new Scanner(System.in);
+		
+		// In ra thông tin, mời User nhập số thứ tự
+		System.out.print("Mời nhập số thứ nhất:");
+			num1 = BanPhim.nextDouble();
+		System.out.print("Mời nhập số thứ hai:");
+			num2 = BanPhim.nextDouble();
+			
 	}
 
 }
