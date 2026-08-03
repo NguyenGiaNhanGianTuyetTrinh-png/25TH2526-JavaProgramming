3b3a5d9a29d11f44119e47696a8c5031ce19f842
NguyenGiaNhanGianTuyetTrinh-png <trinh.ngngt.cc25tth@ntu.edu.vn>
2026-08-03 15:13:19 +0700

    Xong chức năng Xuất danh sách

---
+++ Lab5_ArrayListSoNguyen/src/ntu/trinhngngt/runMain.java
@@ -0,0 +1,41 @@
+package ntu.trinhngngt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class runMain {
+
+	public static void main(String[] args) {
+		// Khai báo biến danh sách, chứa các số nguyên
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+		
+		//a. Nhập 10 phần tử từ bàn phím 
+		Scanner BanPhim = new Scanner(System.in);
+		
+		for(int i=0; i<10; i++ ) {
+			// Nhập 1 phần tử 
+			// in ra một hướng dẫn cho người dùng
+			//System.out.print("Mời nhập phần tử thứ " + i + ":");
+			System.out.print("a[" + i +"]=");
+			// Cho user nhập, cất vào biến tạm
+			Integer tam = BanPhim.nextInt();
+			// Thêm vào cuối danh sách
+			dsSoNguyen.add(tam);
+			
+		}
+				
+		//b. In ra danh sách vừa nhập
+		System.out.print("DANH SÁCH VỪA NHẬP LÀ \n");
+		//for (int i = 0; i<dsSoNguyen.size(); i++) {
+		// Lấy giá trị phần tử ở vị trí i, cất vào biến tạm
+		//Integer tam = dsSoNguyen.get(i);
+		//System.out.print(tam+" ");
+			
+//	}
+		for (Integer x: dsSoNguyen) {
+		System.out.print(x + " ");
+		}
+		
+	}
+			
+}
