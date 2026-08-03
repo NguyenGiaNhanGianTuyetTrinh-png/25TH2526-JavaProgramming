2935e1b130ef48563dfa44fed4090951cfe84476
NguyenGiaNhanGianTuyetTrinh-png <trinh.ngngt.cc25tth@ntu.edu.vn>
2026-08-03 16:04:38 +0700

    Hoàn thiện BaiTap3_TheTich

---
+++ BaiTap3_TheTich/src/BaiTap3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BaiTap3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap canh khoi lap phuong: ");
+        double canh = sc.nextDouble();
+
+        double theTich = canh * canh * canh;
+
+        System.out.println("The tich khoi lap phuong: " + theTich);
+
+        sc.close();
+    }
+}
